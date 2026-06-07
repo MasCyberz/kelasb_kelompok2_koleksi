e2c4a360d487fbfa3b03b6022d29bf3e306a3f73
kengayuh5-arch <kengayuh5@gmail.com>
2026-06-07 21:32:10 +0700

    Update kolekso_repo

--- repositories/koleksi_repo.py
+++ repositories/koleksi_repo.py
@@ -2,29 +2,33 @@
 # Nama : Ken Gayuh Nusa Islami
 # NIM  : K3525031
 
-from .interface import KoleksiRepository
-from models.koleksi import 
+
+from repositories.interface import KoleksiRepository
+from models.koleksi import Koleksi
 
 
 class KoleksiRepositoryImpl(KoleksiRepository):
     def __init__(self):
         self._data = []
 
-    def tambah(self, koleksi):
+    def tambah(self, koleksi: Koleksi):
         for existing in self._data:
-            if existing.kode == koleksi.kode:
-                print("Error: Kode {} sudah ada!".format(koleksi.kode))
+            if existing.kode_koleksi == koleksi.kode_koleksi:
+                print("Error: Kode {} sudah ada!".format(koleksi.kode_koleksi))
                 return False
+            
         self._data.append(koleksi)
         return True
 
-    def hapus(self, kode):
+    def hapus(self, kode: str):
         for i, koleksi in enumerate(self._data):
-            if koleksi.kode == kode:
+            if koleksi.kode_koleksi == kode:
                 self._data.pop(i)
                 return True
+            
         print("Error: Kode {} tidak ditemukan!".format(kode))
         return False
+
 
     def get_semua(self):
         return self._data[:]
