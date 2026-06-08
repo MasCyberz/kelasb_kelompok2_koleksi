0d87309e19a83cec86e2f15f1a4d8e61488aa29e
BitangFajar-blip <fajarbintang057@student.uns.ac.id>
2026-06-08 11:31:13 +0700

    update jurnal.py

--- models/jurnal.py
+++ models/jurnal.py
@@ -1,7 +1,13 @@
-#kode Sementara
-#from .koleksi import Koleksi
+# ==========================================
+# Nama Anggota : Bintang Fajar Khoiru Rizal
+# NIM          : K3525001
+# Bagian Tugas : jurnal.py
+# ==========================================
+
+from models.koleksi import Koleksi
 
 class Jurnal(Koleksi):
+    # Class Jurnal yang mewarisi Koleksi
     def __init__(
         self,
         kode_koleksi,
@@ -11,17 +17,12 @@ class Jurnal(Koleksi):
         bidang_studi,
         impact_factor
     ):
-        super().__init__(
-            kode_koleksi,
-            judul,
-            tahun_terbit,
-            penerbit
-        )
-
+        super().__init__(kode_koleksi, judul, tahun_terbit, penerbit)
         self.bidang_studi = bidang_studi
         self.impact_factor = impact_factor
 
     def tampilkan_detail(self):
+        # Mengisi fungsi dari induk dan menghasilkan data berbentuk dictionary
         return {
             "Jenis": "Jurnal",
             "Kode": self.kode_koleksi,
